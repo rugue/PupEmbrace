1358bfd280c497670b2c75a0ded4e24cb4cca10b
rugue <nehirugue@gmail.com>
2021-03-07 22:45:54 +0100

    Navigating from Overview to details Screen

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,20 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -36,26 +45,53 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-// Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "overview",
+        builder = {
+            composable("overview", content = { Overview(navController = navController) })
+            composable("details", content = { Details(navController = navController) })
+        }
+    )
+}
+
+@Composable
+fun Overview(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Overview Screen")
+        Spacer(modifier = Modifier.padding(10.dp))
+        Button(onClick = {
+            navController.navigate("details")
+        }) {
+            Text("Go To Details Screeen", fontSize = 20.sp, color = Color.White)
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
-}
-
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+fun Details(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Details Screen")
+        Spacer(modifier = Modifier.padding(10.dp))
+        Button(onClick = {
+            navController.navigate("overview") {
+                popUpTo = navController.graph.startDestination
+                launchSingleTop = true
+            }
+        }) {
+            Text("Go To Overview Screen", fontSize = 20.sp, color = Color.White)
+        }
     }
 }
